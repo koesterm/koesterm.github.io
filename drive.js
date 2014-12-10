1175f46a1c3ca8feb241bc07d8e5cd0577d074db
koesterm <koesterm@purdue.edu>
2014-12-10 11:13:38 -0500

    Update drive.js

--- drive.js
+++ drive.js
@@ -96,7 +96,7 @@ var atom = ["<?xml version='1.0' encoding='UTF-8'?>",
   
   $.ajax({
 		type:"POST",
-		url: '"https://spreadsheets.google.com/feeds/cells/1wrTuRSXhuzu50ozWG5RcTeolnGe3GCnCCYtvpLoTGP0/o3o7gnr/private/full"',
+		url: 'https://spreadsheets.google.com/feeds/cells/1wrTuRSXhuzu50ozWG5RcTeolnGe3GCnCCYtvpLoTGP0/o3o7gnr/private/full',
 		crossDomain: true,
 		data: atom,
 		dataType: 'xml',
