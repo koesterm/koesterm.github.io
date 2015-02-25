10fc117533a153a446a934067882bea1835c9564
koesterm <koesterm@purdue.edu>
2015-02-25 13:37:14 -0500

    Update data_store.js

--- data_store.js
+++ data_store.js
@@ -352,8 +352,39 @@ function spTableClickListener(){
     calculateSpeed();
 }    
 
+(function($) {
+     $.fn.doubleTap = function(doubleTapCallback) {
+         return this.each(function(){
+			var elm = this;
+			var lastTap = 0;
+			$(elm).bind('vmousedown', function (e) {
+                                var now = (new Date()).valueOf();
+				var diff = (now - lastTap);
+                                lastTap = now ;
+                                if (diff < 250) {
+		                    if($.isFunction( doubleTapCallback ))
+		                    {
+		                       doubleTapCallback.call(elm);
+		                    }
+                                }      
+			});
+         });
+    }
+})(jQuery);
+
+
+
+
+
+
+
+
+//  put class="doubleTap" on the elements you need to double tap
+$(".doubleTap").doubleTap(function(){
+			// 'this' is the element that was double tap
+  });
 function spreaderTableDblClick(){
-    $('#spTable').find('tr').deligat("dblclick doubletap", function(event){
+    $('#spTable').find('tr').doubleTap(function(){
         $(this).siblings().removeClass("highlighted");
         $(this).toggleClass("highlighted");
         var cur_spreader_name = $("#spTable tr.highlighted td")[0].innerHTML;
