a1d1d08ef2b4896233e07cb38b0848e44a5be546
koesterm <koesterm@purdue.edu>
2015-02-25 13:24:21 -0500

    Update data_store.js

--- data_store.js
+++ data_store.js
@@ -353,7 +353,7 @@ function spTableClickListener(){
 }    
 
 function spreaderTableDblClick(){
-    $('#spTable').find('tr').dblclick(function(){
+    $('#spTable').find('tr').doubletap(function(){
         $(this).siblings().removeClass("highlighted");
         $(this).toggleClass("highlighted");
         var cur_spreader_name = $("#spTable tr.highlighted td")[0].innerHTML;
